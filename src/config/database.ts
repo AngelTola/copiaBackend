20e8fb95bcf68e8293c6f8809937099460cfb0a2
AngelTola <tolaangel606@gmail.com>
2025-06-14 04:23:29 -0400

    intento 4

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,4 +1,12 @@
+/*
+Vista rápida del funcionamiento y contenido de esta carpeta
 
+    Archivos de configuración del proyecto
+        - Conexión a la base de datos
+        - Configuración de variables de entorno, servicios externos y/o opciones del servidor
+*/
+
+// El error sale porque Prisma no generará un cliente si no hay modelos definidos
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import session from "express-session";
 import passport from "passport";
 import path from "path";
 import { PrismaClient } from "@prisma/client";
+import { v4 as uuidv4 } from "uuid";
 
 // Configuración de Google Auth
 import "./config/googleAuth";
